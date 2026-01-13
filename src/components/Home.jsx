3a9131a35a03eda7f218057a0d84b61585086d04
harshitasalvi-sa <harshita.salvi@startappss.com>
2026-01-13 11:05:05 +0530

    Added home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Home.css"
+const Home = () => {
+  return (
+    <>
+        <h1>Projects</h1>
+
+        <div className="container">
+            <div className="project">
+
+            </div>
+            <div className="project">
+
+            </div>
+            <div className="project">
+
+            </div>
+            <div className="project">
+
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
